a4e7ff48bc8f5abbb36e3b27dd24dee66b298c57
Jonathan Geraldo <j.geraldo.romero@gmail.com>
2023-06-06 17:18:28 -0400

    primera carga

---
+++ example.py
@@ -0,0 +1,11 @@
+#type() devuelve el tipo del valor/variable entregado
+#\n salto de linea
+#\t tabulacion
+#se debe anteponer un \ si una string dentro de comillas lleva comilla del mismo tipo ejemplo: "esa \"cosa\""
+
+name =input("Ingresa tu nombre: ")
+mensaje = str("Hola "+name)
+
+print(mensaje)
+print("Bienvenido!!...")
+#print("var mensaje = "+str(type(mensaje)))
