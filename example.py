dd2355486ece0b0752bdec319c99b4c18c5327b2
Jonathan Geraldo <j.geraldo.romero@gmail.com>
2023-06-06 18:07:20 -0400

    ejercicios de prueba

--- example.py
+++ example.py
@@ -3,9 +3,24 @@
 #\t tabulacion
 #se debe anteponer un \ si una string dentro de comillas lleva comilla del mismo tipo ejemplo: "esa \"cosa\""
 
-name =input("Ingresa tu nombre: ")
-mensaje = str("Hola "+name)
+#name =input("Ingresa tu nombre: ")
+#mensaje = str("Hola "+name)
 
-print(mensaje)
-print("Bienvenido!!...")
+name = ""
+
+while name == "":
+    name =input("Ingresa tu nombre: ")
+    mensaje = str("Hola " + name)
+    if name != "":
+        print(mensaje)
+        print("Bienvenido!!...")
+    else:
+        name = input("Vuelve a ingresar tu nombre: ")
+        mensaje = str("Hola " + name)
+        if name != "":
+            print(mensaje)
+            print("Bienvenido!!...")
+        else:
+            print("No has ingresado tu nombre")
+
 #print("var mensaje = "+str(type(mensaje)))
